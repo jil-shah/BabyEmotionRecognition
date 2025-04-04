3f6e057ae4a29783abf32739c106bac7ebc687c0
Shiv Thakar <94186009+ShivThakar18@users.noreply.github.com>
2025-04-03 21:28:03 -0400

    Update BabyBeacon.sh

--- BabyBeacon.sh
+++ BabyBeacon.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Open terminal for C++ program
-gnome-terminal -- bash -c "cd BabyEmotionRecognition && make && ./BabyBeacon; exec bash"
-
 # Open terminal for Python script in virtual environment
-gnome-terminal -- bash -c "cd ~ && source ../tf-venv/bin/activate && cd /home/raspberry/BabyEmotionRecognition/python && python script.py; exec bash"
+lxterminal --working-directory=/home/raspberry/BabyEmotionRecognition/python --command="bash -c 'source /home/raspberry/tf-venv/bin/activate && python main.py; exec bash'"
+
+# Wait 3 seconds before starting Python
+sleep 3
+
+# Open terminal for C++ program
+lxterminal --working-directory=/home/raspberry/BabyEmotionRecognition --command="bash -c 'make && ./BabyBeacon; exec bash'"
